a2915cb1fff8c7fd301e1ed0ad15b262940f2f07
Blueagle <austinliu@blueagle.top>
2022-01-08 20:40:16 +0800

    t

--- src/check/login.js
+++ src/check/login.js
@@ -2,21 +2,31 @@ import { dialog } from 'utils';
 import { message } from 'message';
 
 export default async function () {
-  const isLogin = JSON.parse(document.getElementById('__NEXT_DATA__').innerText).props.initialState.user.isAuthenticated;
-  const isLoginPage = window.location.pathname === '/login';
-  if (!isLogin && !isLoginPage) {
-    dialog({
-      content: message.loginGuide.content,
-      actions: [
-        {
-          label: message.loginGuide.no,
-          onClick: () => { },
-        },
-        {
-          label: message.loginGuide.yes,
-          onClick: () => window.location.href = `/login?returnUrl=${window.location.pathname}`,
-        }
-      ]
-    })
+
+  function check() {
+    const isLogin = JSON.parse(document.getElementById('__NEXT_DATA__').innerText).props.initialState.user.isAuthenticated;
+    const isLoginPage = window.location.pathname === '/login';
+    if (!isLogin && !isLoginPage) {
+      dialog({
+        content: message.loginGuide.content,
+        actions: [
+          {
+            label: message.loginGuide.no,
+            onClick: () => { },
+          },
+          {
+            label: message.loginGuide.yes,
+            onClick: () => window.location.href = `/login?returnUrl=${window.location.pathname}`,
+          }
+        ]
+      })
+    }
   }
+
+  if (document.body) {
+    check()
+  } else {
+    window.addEventListener('DOMContentLoaded', check);
+  }
+
 }
