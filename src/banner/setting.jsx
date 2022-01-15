ecd58706cc7e1e1340061125e6e6923e9f995670
Blueagle <austinliu@blueagle.top>
2022-01-16 00:43:29 +0800

    t

--- src/banner/setting.jsx
+++ src/banner/setting.jsx
@@ -3,6 +3,8 @@ import { Fragment } from 'preact';
 import { useState } from 'preact/hooks';
 import { useConfig } from '../config';
 
+import { styled } from '@mui/material/styles';
+
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 
@@ -50,6 +52,38 @@ export default function () {
   const handleChangeTab = (event, newValue) => {
     setTab(newValue);
   };
+  const Android12Switch = styled(Switch)(({ theme }) => ({
+    padding: 8,
+    '& .MuiSwitch-track': {
+      borderRadius: 22 / 2,
+      '&:before, &:after': {
+        content: '""',
+        position: 'absolute',
+        top: '50%',
+        transform: 'translateY(-50%)',
+        width: 16,
+        height: 16,
+      },
+      '&:before': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+          theme.palette.getContrastText(theme.palette.primary.main),
+        )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+        left: 12,
+      },
+      '&:after': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+          theme.palette.getContrastText(theme.palette.primary.main),
+        )}" d="M19,13H5V11H19V13Z" /></svg>')`,
+        right: 12,
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxShadow: 'none',
+      width: 16,
+      height: 16,
+      margin: 2,
+    },
+  }));
   return (
     <Fragment>
       <IconButton onClick={handleOpen}>
@@ -76,7 +110,7 @@ export default function () {
                   <ListItem>
                     <ListItemButton onClick={() => handleToggleConfig(id)}>
                       <ListItemText primary={label} secondary={description} />
-                      <Switch
+                      <Android12Switch
                         edge="end"
                         checked={enabled}
                       />
